f2748bbd4feb9196f5bf8546d6f672dd9fc9bc94
David White <dwhite@barkleyrei.com>
2023-06-27 16:43:56 -0400

    fix: CWU-100 CWU-101 CWU-102 CWU-103 library bug fixes

--- src/static/assets/ejs/modules/chat.js
+++ src/static/assets/ejs/modules/chat.js
@@ -22,6 +22,9 @@ const chat = {
 						return (txt === 'Click here to open the library chat window') ? txt.replace('open', 'close') : txt.replace('close', 'open');
 					}
 				});
+				chat.elem.$chat.find('.chat__button span').text(function() {
+					return(this.textContent === 'Ask Here') ? 'Close Chat' : 'Ask Here';
+				});
 				chat.elem.$chat.find('.chat__window').attr({
 					'aria-hidden': function (i, txt) {
 						return (txt === 'false') ? 'true' : 'false';
@@ -33,6 +36,7 @@ const chat = {
 				e.preventDefault();
 				chat.elem.$chat.addClass('chat--open');
 				chat.elem.$chat.find('.chat__button').attr({ 'aria-expanded': 'true', 'aria-label': 'Click here to close the library chat window' });
+				chat.elem.$chat.find('.chat__button span').text('Close Chat');
 				chat.elem.$chat.find('.chat__window').attr({'aria-hidden': 'false' });
 				chat.elem.$chat.find('iframe').focus();				
 			});
