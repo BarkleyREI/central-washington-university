891e745b8e7aa05064d3e9d2333ec0571fa6efae
David White <dwhite@barkleyrei.com>
2023-08-30 16:14:28 -0400

    fix: stripped down main.js for squiz

--- src/static/assets/ejs/squiz.js
+++ src/static/assets/ejs/squiz.js
@@ -1,11 +1,13 @@
 import { ready } from 'brei-project-utils';
 
+import { accordion } from './modules/accordion';
 import { header } from './modules/header';
 import { search } from './modules/search';
 
 const main = {
 
 	init() {
+		accordion.init();
 		header.init();
 		search.init();
 	},
